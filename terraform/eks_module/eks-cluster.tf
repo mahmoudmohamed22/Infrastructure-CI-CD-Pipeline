3eb0523851b84975a9986bec639d4834ebb982f6
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-05-26 22:33:13 +0300

    create init eks module

---
+++ terraform/eks_module/eks-cluster.tf
@@ -0,0 +1,18 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.eks_name
+  role_arn = aws_iam_role.eks_role.arn
+ 
+  vpc_config {
+    subnet_ids = [var.private_sub_1, var.private_sub_2]
+    endpoint_public_access = false
+    endpoint_private_access= true
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
+    
+  ]
+}
+
