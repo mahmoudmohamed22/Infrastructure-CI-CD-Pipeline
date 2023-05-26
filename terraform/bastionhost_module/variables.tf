516b04c7f6ad8734058acd62878f3fc3ed293357
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-05-26 22:32:25 +0300

    create bastion host module

---
+++ terraform/bastionhost_module/variables.tf
@@ -0,0 +1,28 @@
+ variable "test_instance_profile_name" {
+    type=string
+  }
+
+
+   variable "bastionhost_subnet_id" {
+    type=string
+  }
+
+   variable "security_group_id" {
+    type=string
+  }
+  
+     variable "eks_cluster_name" {
+    type=string
+  }
+
+  variable "aws_secretsmanager_secret_version_secret_version_secretsmanager_vv" {
+    type=string
+  }
+
+    variable "key_name" {
+    type=string
+  }
+
+
+# variable terraform_user_secret_arn {type=string}
+
