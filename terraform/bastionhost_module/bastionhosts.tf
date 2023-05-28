73aed2bcd9ac03155ca59e8f5cd7ca6ed859952e
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-05-28 21:32:33 +0300

    move deployments files and install aws and kubectl

--- terraform/bastionhost_module/bastionhosts.tf
+++ terraform/bastionhost_module/bastionhosts.tf
@@ -16,17 +16,44 @@ resource "aws_instance" "bastionhost1" {
   key_name                = var.key_name
   user_data = <<-EOF
               #!/bin/bash
+              #copy key from secretmanager to machine
               echo $(aws secretsmanager get-secret-value --secret-id my-private-key --query SecretString --output text) > /home/ec2-user/${var.key_name}.pem 
               chown ec2-user:ec2-user ${var.key_name}.pem
               chmod 400 /home/ec2-user/${var.key_name}.pem
-              curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-              chmod +x ./kubectl
-              sudo mv ./kubectl /usr/local/bin/kubectl
+             
+              # install kubectl
+              
+              curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+              sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+              chmod +x kubectl
+              mkdir -p ~/.local/bin
+              mv ./kubectl ~/.local/bin/kubectl
+
+              # install aws cli 
+              sudo apt-get install unzip
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
+
+
               # sudo mkdir /home/ec2-user/.aws/
               # sudo touch /home/ec2-user/.aws/credentials
-              # echo "[default]" > /home/ec2-user/.aws/credentials
+              
               aws eks update-kubeconfig --region us-east-1 --name ${var.eks_cluster_name}
               EOF
+   
+  provisioner "file" {
+              connection {
+                    type        = "ssh"
+                    user        = "ubuntu"
+                    private_key = file("~/Documents/Infrastructure-ITI-DevOps/terraform/tf_key.pem")
+                    host        = self.public_ip
+                  }
+    source      = "~/Documents/Infrastructure-ITI-DevOps/Deploy Jenkins on EKS"
+    destination = "~/jenkin-k8s"
+  }
+
+
   tags = {
     Name = "bastion-host"
   }
