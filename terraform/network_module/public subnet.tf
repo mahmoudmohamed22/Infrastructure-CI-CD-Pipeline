fda849f23011fedadd78bbe50bffe9c47a38e4c9
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-05-31 17:01:00 +0300

    fix problem don't create elb for jenkins deploy

--- terraform/network_module/public subnet.tf
+++ terraform/network_module/public subnet.tf
@@ -4,10 +4,11 @@ resource "aws_subnet" "public_sub1" {
   availability_zone = var.AZ_1
   map_public_ip_on_launch = true
 
+
   tags = {
     Name = "${var.COMPANY_NAME}_pub-subent-1"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
+    # "kubernetes.io/role/elb"     = "1"
+    # "kubernetes.io/cluster/demo" = "owned"
   }
 }
 
@@ -19,21 +20,9 @@ resource "aws_subnet" "public_sub2" {
  
   tags = {
     Name = "${var.COMPANY_NAME}_pub-subent-2"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
+    # "kubernetes.io/role/elb"     = "1"
+    # "kubernetes.io/cluster/demo" = "owned"
   }
 }
 
 
-# resource "aws_subnet" "public_sub3" {
-#   vpc_id     = aws_vpc.vpc_main.id
-#   cidr_block = var.PUBLIC_SUBNET_3_CIDR
-#   availability_zone = var.AZ_3
-#   map_public_ip_on_launch = true
- 
-#   tags = {
-#     Name = "${var.COMPANY_NAME}_pub-subent-3"
-#     "kubernetes.io/role/elb"     = "1"
-#     "kubernetes.io/cluster/demo" = "owned"
-#   }
-# }
