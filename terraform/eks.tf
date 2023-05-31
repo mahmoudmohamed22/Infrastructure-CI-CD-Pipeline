34fae7181037e91cb22e571cf6579431bb9d8333
mahmoudmohamed22 <eng.mahmoud.elghonemy@gmail.com>
2023-05-31 17:06:15 +0300

    pass new values for new vars eks modules

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,6 +5,10 @@ private_sub_1= module.my_network.priv_sub_1
 private_sub_2 =module.my_network.priv_sub_2
 public_sub_1= module.my_network.pub_sub_1
 public_sub_2 =module.my_network.pub_sub_2
+VPC_CIDR=var.VPC_CIDR
+vpc_id=module.my_network.vpc_id
+bastionhost-SG-id=module.my_network.public_SG
 
-
+key=module.my_secrets_manager.key_name
+public_sub_cidr=module.my_network.public_sub_cidr
 }
